9953c75bbe05b413a79cf4c7df237e0601fefe36
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-02-27 16:25:27 -0800

    Project 1 Uploaded

---
+++ Project 1/revised.cpp
@@ -0,0 +1,18 @@
+// Project 1: Apurva Shah - 100%
+#include <iostream>
+int main()
+{
+double r, r_squared, test;
+std::cout << "what is the value for r ?\n";
+std::cin >> r;
+r_squared = r * r;
+std::cout << "r-squared = " << r_squared << std::endl;
+double vif = 1 / (1 - r_squared);
+std::cout << "vif = " << vif << std::endl;
+test = 1 - vif;
+std::cout << "test = " << test << std::endl;
+if (vif > 1) {
+std::cout << "vif is greater than 1"; }
+else {
+std::cout << "vif is less than 1"; }
+return 0; }
