da4aa11eca9a599d58a004489be8b916dc7ce150
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-03-08 21:22:09 -0800

    Took lecture notes & worked with some pointers

--- LectureNotes/lecture_notes.cpp
+++ LectureNotes/lecture_notes.cpp
@@ -9,27 +9,17 @@ using namespace std;
 
 int main(){
 
-    vector<int> Score; // specifiying the datatype and the name
-     // defining an integer vector named score
+   char blocks[ 3 ] = {'A','B','C'};
+   char *ptr = &blocks[0]; 
+   cout << ptr << endl;
+   char temp;
+    temp = blocks[ 0 ];
+    cout << temp << endl;
+    temp = *ptr;
+cout << temp << endl;
 
-   //  Score[0] = 1; // this doesnt necessarily work,
-    // you have to use the pushback feature to initially 
-    // initialize it
 
-    for (int i = 0; i<=9; i++) {
-        Score.push_back(i);
-    }
 
-    // Sice we initialized 9 elements, 16 elements are allocated
-
-    for (int i = 0; i<=9; i++) {
-        cout << Score[i] << endl;
-    }
-
-    // returns how many elements allocated
-    cout << Score.capacity() << endl;
-    // returns initalized elements
-    cout << Score.size() << endl;
 
     return 0;
 }
